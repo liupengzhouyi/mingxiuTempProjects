85cc224ece4f258b51887a45b4766526a80fb485
liupeng <LIUPENG.0@outlook.com>
2019-04-19 16:42:31 +0800

    文件上传页面 V 1.0.4

--- src/ServletPackage/UploadImageServlet.java
+++ src/ServletPackage/UploadImageServlet.java
@@ -7,13 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "UploadImageServlet")
+@WebServlet(name = "/UploadImageServlet")
 public class UploadImageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /*设置编码*/
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text;charset=UTF-8");
+
+
+        
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request, response);
     }
 }
